74a3ac21e7b40dfe5a52d9b1fa3e244c7a608e39
Bogdan <74682561+inspiq@users.noreply.github.com>
2022-11-12 16:31:31 +0400

    dung buyer

---
+++ AutoBuyer/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace AutoBuyer
+{
+    partial class App
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btn_options = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btn_options
+            // 
+            this.btn_options.BackColor = System.Drawing.Color.Brown;
+            this.btn_options.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_options.BackgroundImage")));
+            this.btn_options.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_options.FlatAppearance.BorderSize = 0;
+            this.btn_options.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_options.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_options.Location = new System.Drawing.Point(194, 42);
+            this.btn_options.Name = "btn_options";
+            this.btn_options.Size = new System.Drawing.Size(160, 35);
+            this.btn_options.TabIndex = 2;
+            this.btn_options.UseVisualStyleBackColor = false;
+            this.btn_options.MouseEnter += new System.EventHandler(this.btn_options_MouseEnter);
+            this.btn_options.MouseLeave += new System.EventHandler(this.btn_options_MouseLeave);
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.Color.Transparent;
+            this.btn_close.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_close.BackgroundImage")));
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.FlatAppearance.BorderSize = 0;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Location = new System.Drawing.Point(32, 300);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(80, 35);
+            this.btn_close.TabIndex = 3;
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(407, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DUNG BUYER v1.0 CREATED BY INSPIQ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Demi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGray;
+            this.label2.Location = new System.Drawing.Point(370, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "INTERVAL";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(513, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Demi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGray;
+            this.label3.Location = new System.Drawing.Point(370, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "START BUTTON";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.Location = new System.Drawing.Point(513, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Demi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.DarkGray;
+            this.label4.Location = new System.Drawing.Point(370, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "STOP BUTTON";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox3.Location = new System.Drawing.Point(513, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.Transparent;
+            this.btn_start.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_start.BackgroundImage")));
+            this.btn_start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_start.FlatAppearance.BorderSize = 0;
+            this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start.Location = new System.Drawing.Point(32, 259);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(80, 35);
+            this.btn_start.TabIndex = 11;
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            this.btn_start.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_start_KeyUp);
+            this.btn_start.MouseEnter += new System.EventHandler(this.btn_start_MouseEnter);
+            this.btn_start.MouseLeave += new System.EventHandler(this.btn_start_MouseLeave);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_options);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "App";
+            this.Text = "DUNG BUYER v1.0";
+            this.Load += new System.EventHandler(this.App_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btn_options;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ AutoBuyer/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace AutoBuyer
+{
+    public partial class App : Form
+    {
+        globalKeyboardHook gkh = new globalKeyboardHook();
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMouse()
+        {
+            int c = 0;
+
+            POINT p = new POINT();
+
+            while (true)
+            {
+                GetCursorPos(ref p);
+                ClientToScreen(Handle, ref p);
+                DoMouseLeftClick(p.x, p.y);
+
+                c++;
+
+                if (c == 1)
+                    break;
+            }
+        }
+
+        [DllImport("user32.dll")]
+
+        public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
+
+        [StructLayout(LayoutKind.Sequential)]
+
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [DllImport("user32.dll")]
+
+        public static extern void mouse_event(int dsFlags, int dx, int dy, int cButtons, int dsExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        private void DoMouseLeftClick(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        }
+
+        [DllImport("user32.dll")]
+
+        public static extern bool GetCursorPos(ref POINT lpPoint);
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(1475, 950);
+            ClickMouse();
+            SendKeys.Send("9");
+            SendKeys.Send("9");
+            Cursor.Position = new Point(1475, 915);
+            ClickMouse();
+            ClickMouse();
+        }
+
+        private void App_Load(object sender, EventArgs e)
+        {
+            gkh.HookedKeys.Add(Keys.F10);
+            gkh.HookedKeys.Add(Keys.F11);
+            gkh.KeyUp += new KeyEventHandler(btn_start_KeyUp);
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+        }
+
+        private void btn_start_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F10)
+            {
+                timer.Enabled = true;
+            }
+            if (e.KeyCode == Keys.F11)
+            {
+                timer.Enabled = false;
+            }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_options_MouseEnter(object sender, EventArgs e)
+        {
+            btn_options.BackColor = Color.Brown;
+        }
+
+        private void btn_options_MouseLeave(object sender, EventArgs e)
+        {
+            btn_options.BackColor = Color.Brown;
+        }
+
+        private void btn_start_MouseLeave(object sender, EventArgs e)
+        {
+            btn_start.BackColor = Color.Transparent;
+        }
+
+        private void btn_start_MouseEnter(object sender, EventArgs e)
+        {
+            btn_start.BackColor = Color.White;
+        }
+
+        class globalKeyboardHook
+        {
+            #region Constant, Structure and Delegate Definitions
+
+            public struct keyboardHookStruct
+            {
+                public int vkCode;
+                public int scanCode;
+                public int flags;
+                public int time;
+                public int dwExtraInfo;
+            }
+
+            const int WH_KEYBOARD_LL = 13;
+            const int WM_KEYDOWN = 0x100;
+            const int WM_KEYUP = 0x101;
+            const int WM_SYSKEYDOWN = 0x104;
+            const int WM_SYSKEYUP = 0x105;
+            #endregion
+
+            #region Instance Variables
+            public List<Keys> HookedKeys = new List<Keys>();
+            IntPtr hhook = IntPtr.Zero;
+            #endregion
+
+            #region Events
+
+            public event KeyEventHandler KeyDown;
+
+            public event KeyEventHandler KeyUp;
+            #endregion
+
+            #region Constructors and Destructors
+
+            public globalKeyboardHook()
+            {
+                hook();
+            }
+
+            ~globalKeyboardHook()
+            {
+                unhook();
+            }
+            #endregion
+
+            #region Public Methods
+ 
+            public void hook()
+            {
+                IntPtr hInstance = LoadLibrary("User32");
+                delegateHookProc = hookProc;
+                hhook = SetWindowsHookEx(WH_KEYBOARD_LL, delegateHookProc, hInstance, 0);
+            }
+
+            public delegate int keyboardHookProc(int code, int wParam, ref keyboardHookStruct lParam);
+            keyboardHookProc delegateHookProc;
+
+            public void unhook()
+            {
+                UnhookWindowsHookEx(hhook);
+            }
+
+            public int hookProc(int code, int wParam, ref keyboardHookStruct lParam)
+            {
+                if (code >= 0)
+                {
+                    Keys key = (Keys)lParam.vkCode;
+                    if (HookedKeys.Contains(key))
+                    {
+                        KeyEventArgs kea = new KeyEventArgs(key);
+                        if ((wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) && (KeyDown != null))
+                        {
+                            KeyDown(this, kea);
+                        }
+                        else
+                        if ((wParam == WM_KEYUP || wParam == WM_SYSKEYUP) && (KeyUp != null))
+                        {
+                            KeyUp(this, kea);
+                        }
+                        if (kea.Handled)
+                            return 1;
+                    }
+                }
+                return CallNextHookEx(hhook, code, wParam, ref lParam);
+            }
+            #endregion
+
+            #region DLL imports
+
+            [DllImport("user32.dll")]
+            static extern IntPtr SetWindowsHookEx(int idHook, keyboardHookProc callback, IntPtr hInstance, uint threadId);
+            [DllImport("user32.dll")]
+            static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+
+            [DllImport("user32.dll")]
+            static extern int CallNextHookEx(IntPtr idHook, int nCode, int wParam, ref keyboardHookStruct lParam);
+
+            [DllImport("kernel32.dll")]
+            static extern IntPtr LoadLibrary(string lpFileName);
+            #endregion
+        }
+    }
+}
